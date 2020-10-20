0e0e370d99828bf21143d5eb70d557fad4018138
anamfazal <anamfazal94@gmail.com>
2020-10-20 14:26:28 +0530

    Added : code to add hotel

--- src/com/hotelreservationprogram/HotelReservation.java
+++ src/com/hotelreservationprogram/HotelReservation.java
@@ -1,7 +1,17 @@
 package com.hotelreservationprogram;
+import java.util.Arrays;
 
 public class HotelReservation {
+    public Hotel[] hotel = new Hotel[3];
+
     public static void main(String[] args){
-        System.out.println("Welcome to hotel reservation program");
+           HotelReservation hotelReservation = new HotelReservation();
+           Hotel hotel1 = new Hotel("Lakewood" , 110 ,80);
+           hotelReservation.hotel[0] = hotel1;
+           Hotel hotel2 = new Hotel("Bridgewood",160 ,110);
+           hotelReservation.hotel[1] = hotel2;
+           Hotel hotel3 = new Hotel("Bridgewood",220 ,100);
+           hotelReservation.hotel[2] = hotel3;
+
     }
 }
