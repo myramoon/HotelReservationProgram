36ce4e47968ef776ef3ada86c627823217e49ec3
anamfazal <anamfazal94@gmail.com>
2020-10-20 14:27:17 +0530

    Added : code to add hotel

---
+++ src/com/hotelreservationprogram/Hotel.java
@@ -0,0 +1,12 @@
+package com.hotelreservationprogram;
+
+public class Hotel {
+    public String name;
+    public int regDayRate;
+    public int rewDayRate;
+    public Hotel(String name , int regDayRate , int rewDayRate){
+        this.name = name;
+        this.regDayRate = regDayRate;
+        this.rewDayRate = rewDayRate;
+    }
+}
